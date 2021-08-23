11348f4691ba55e67d7b98c50ad6c2a6852a4ce0
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2021-08-23 16:43:57 -0500

    console.log work

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
+var playerName = window.prompt("What is your robot's name?");
+// what is this?
+console.log(playerName);
+console.log("This logs a sting, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("our robot's name is " + playerName);
